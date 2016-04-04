341b0eb80e2a8db1a82bf604d587a7d0f5a2c983
dimnikolos <dnikolos@gmail.com>
2016-04-04 14:10:43 +0300

    wishful thinking

--- sampleExtension.js
+++ sampleExtension.js
@@ -1,4 +1,22 @@
 (function(ext) {
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+    window.AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext || window.msAudioContext;
+    navigator.getUserMedia(
+        {audio : true},
+        function(stream) {
+
+          var url = URL.createObjectURL(stream);
+          var audioContext = new AudioContext();
+          var mediastreamsource = audioContext.createMediaStreamSource(stream);
+          var analyser = audioContext.createAnalyser();
+          var frequencyData = new Uint8Array(analyser.frequencyBinCount);
+          mediastreamsource.connect(analyser);
+      },
+      function(e) {
+          console.log(e);
+      }
+      );
+
     // Cleanup function when the extension is unloaded
     ext._shutdown = function() {
     };
@@ -17,15 +35,17 @@
         console.log("Hi there!");
     };
 
-    ext.hi = function(base,exponent){
-        return 3;
+    ext.whistle = function(base,exponent){
+        analyser.getByteFrequencyData(frequencyData);
+        return(frequencyData.indexOf(Math.max.apply(Math, frequencyData)));
+
     }
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
         [' ', 'Write to console', 'consoleWrite'],
-        ['r', 'hi', 'hi']
+        ['r', 'whistle','whistle']
 
         ],
         url: 'http://' // Link to extension documentation, homepage, etc.
