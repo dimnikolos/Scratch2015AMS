e984d748998ffdc57f1d412d13de356026c873e8
dimnikolos <dnikolos@gmail.com>
2016-04-04 14:40:27 +0300

    wishful thinking

--- sampleExtension.js
+++ sampleExtension.js
@@ -1,5 +1,4 @@
 (function(ext) {
-    var maxindex;
     navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
     window.AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext || window.msAudioContext;
     navigator.getUserMedia(
